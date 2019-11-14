934acef7c0c9d3bd8ffa1b1a366da7893bebc3f0
Charlie Vorbach <cvorbach@mit.edu>
2019-11-14 12:50:58 -0500

    stereo calibration has 4% density - something broken'

--- notebook.py
+++ notebook.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
+# import sys
+# sys.executable
+
 # setup
 np.set_printoptions(precision=4, suppress=True)
 st.title("Online Calibration")
@@ -32,9 +35,9 @@ img1_raw = np.array(cam1_raw)
 # # display both pairs of images
 "Raw Left Image"
 st.image(img0_raw, use_column_width=True)
-# 
-# st.text("Raw Right Image")
-# st.image(img1_raw, use_column_width=True)
+
+"Raw Right Image"
+st.image(img1_raw, use_column_width=True)
 # 
 # st.text("Rectified Left Image")
 # st.image(img0, use_column_width=True)
@@ -92,15 +95,15 @@ T_01 = data.calib.T_01 # Translation to left image
 # st.text(str(K_01))
 # st.text(str(R_01))
 # st.text(str(T_01))
-# 
-# undistorted_img0 = cv2.undistort(img0_raw, K_00, D_00)
-# undistorted_img1 = cv2.undistort(img1_raw, K_01, D_01)
-# 
-# "Undistorted Left Image"
-# st.image(undistorted_img0, use_column_width=True)
-# 
-# "Undistorted Right Image"
-# st.image(undistorted_img1, use_column_width=True)
+
+undistorted_img0 = cv2.undistort(img0_raw, K_00, D_00)
+undistorted_img1 = cv2.undistort(img1_raw, K_01, D_01)
+
+"Undistorted Left Image"
+st.image(undistorted_img0, use_column_width=True)
+
+"Undistorted Right Image"
+st.image(undistorted_img1, use_column_width=True)
 
 Pr_00 = np.zeros((3,4)) # Left rectify projection matrix
 Pr_01 = np.zeros((3,4)) # Right rectify projection matrix
@@ -121,24 +124,61 @@ Rr_01 = np.zeros((3,3)) # Right rectify rotation
 #                     Pr_01)  
 #         )
 
-cv2.stereoRectify(  K_00, 
-                    D_00, 
-                    K_01, 
-                    D_01, 
-                    img_size,
-                    R_01, 
-                    T_01, 
-                    Rr_00, 
-                    Rr_01, 
-                    Pr_00, 
-                    Pr_01)  
-
-mapx_00, mapy_00 = cv2.initUndistortRectifyMap(K_00, D_00, Rr_00, K_00, img_size, cv2.CV_32FC1)
-
-img0_rect = cv2.remap(img0, mapx_00, mapy_00, cv2.INTER_LINEAR)
-
-st.image(img0_rect, use_column_width=True)
-
-# "Rectified Left Image"
-st.image(img0, use_column_width=True)
-
+# cv2.stereoRectify(  K_00, 
+#                     D_00, 
+#                     K_01, 
+#                     D_01, 
+#                     img_size,
+#                     R_01, 
+#                     T_01, 
+#                     Rr_00, 
+#                     Rr_01, 
+#                     Pr_00, 
+#                     Pr_01,  
+#                     alpha=0,
+#                     )
+# 
+# st.text((
+#     Rr_00,
+#     Rr_01,
+#     Pr_00,
+#     Pr_01,
+# ))
+# 
+# mapx_00, mapy_00 = cv2.initUndistortRectifyMap(K_00, D_00, Rr_00, K_00, img_size, cv2.CV_32FC1)
+# img0_rect = cv2.remap(img0_raw, mapx_00, mapy_00, cv2.INTER_LINEAR)
+# 
+# "Undistorted, Rectified Left Image"
+# st.image(img0_rect, use_column_width=True)
+# 
+# "Kitti Rectified Left Image"
+# st.image(img0, use_column_width=True)
+# 
+# mapx_01, mapy_01 = cv2.initUndistortRectifyMap(K_01, D_01, Rr_01, K_01, img_size, cv2.CV_32FC1)
+# img1_rect = cv2.remap(img1_raw, mapx_01, mapy_01, cv2.INTER_LINEAR)
+# 
+# 'Undistorted, Rectified Right Image'
+# st.image(img1_rect, use_column_width=True)
+# 
+# 'Kitti Rectified Right Image'
+# st.image(img1, use_column_width=True)
+# 
+# # compute disparities
+# stereo = cv2.StereoBM_create()
+# kitti_disparities = stereo.compute(img0, img1)
+# rect_disparities  = stereo.compute(img0_rect, img1_rect)
+# 
+# # display disparity maps
+# "Kitti Rectified Stereo Disparities"
+# plt.imshow(kitti_disparities, cmap='viridis')
+# st.pyplot()
+# 
+# "Rectified, Undistorted Stereo Disparities"
+# plt.imshow(rect_disparities, cmap='viridis')
+# st.pyplot()
+# 
+# kitti_density = float((kitti_disparities >= 0).sum()) / float(kitti_disparities.size)
+# rect_density  = float((rect_disparities  >= 0).sum()) / float(rect_disparities.size)
+# 
+# f"Rectified Pair Disparity Density: {kitti_density:.2%}"
+# f"Raw Pair Disparity Density: {rect_density:.2%}"
