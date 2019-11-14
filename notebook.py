5a0bb1e2b517cb6ef3689d22cdbbee3178ea021e
Charlie <cvorbach@mit.edu>
2019-11-14 00:30:48 -0500

    Progress on rectification

--- notebook.py
+++ notebook.py
@@ -67,11 +67,78 @@ st.image(img0_raw, use_column_width=True)
 #     f
 #     st.text(getattr(data.calib, f))
 
+img_size = data.calib.S_00
+
+# Left image (Which is origin of coordinate system)
 D_00 = data.calib.D_00
 K_00 = data.calib.K_00
+R_00 = data.calib.R_00 # Rotation is identy
+T_00 = data.calib.T_00 # Translation is zero
 
-undistorted_img0 = cv2.undistort(img0_raw, K_00, D_00)
+# Right image
+D_01 = data.calib.D_01
+K_01 = data.calib.K_01
+R_01 = data.calib.R_01 # Rotation to left image
+T_01 = data.calib.T_01 # Translation to left image
 
-"Undistorted Left Image"
-st.image(undistorted_img0, use_column_width=True)
+# "Img0"
+# st.text(str(D_00))
+# st.text(str(K_00))
+# st.text(str(R_00))
+# st.text(str(T_00))
+# 
+# "Img1"
+# st.text(str(D_01))
+# st.text(str(K_01))
+# st.text(str(R_01))
+# st.text(str(T_01))
+# 
+# undistorted_img0 = cv2.undistort(img0_raw, K_00, D_00)
+# undistorted_img1 = cv2.undistort(img1_raw, K_01, D_01)
+# 
+# "Undistorted Left Image"
+# st.image(undistorted_img0, use_column_width=True)
+# 
+# "Undistorted Right Image"
+# st.image(undistorted_img1, use_column_width=True)
+
+Pr_00 = np.zeros((3,4)) # Left rectify projection matrix
+Pr_01 = np.zeros((3,4)) # Right rectify projection matrix
+Rr_00 = np.zeros((3,3)) # Left rectify rotation
+Rr_01 = np.zeros((3,3)) # Right rectify rotation 
+
+# st.text(
+#         (  K_00, 
+#                     K_01, 
+#                     D_00, 
+#                     D_01, 
+#                     tuple(img_size), 
+#                     R_01, 
+#                     T_01, 
+#                     Rr_00, 
+#                     Rr_01, 
+#                     Pr_00, 
+#                     Pr_01)  
+#         )
+
+cv2.stereoRectify(  K_00, 
+                    D_00, 
+                    K_01, 
+                    D_01, 
+                    img_size,
+                    R_01, 
+                    T_01, 
+                    Rr_00, 
+                    Rr_01, 
+                    Pr_00, 
+                    Pr_01)  
+
+mapx_00, mapy_00 = cv2.initUndistortRectifyMap(K_00, D_00, Rr_00, K_00, img_size, cv2.CV_32FC1)
+
+img0_rect = cv2.remap(img0, mapx_00, mapy_00, cv2.INTER_LINEAR)
+
+st.image(img0_rect, use_column_width=True)
+
+# "Rectified Left Image"
+st.image(img0, use_column_width=True)
 
