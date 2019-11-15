a43ce2492453e6701460de362424ddc3624944db
Charlie <cvorbach@mit.edu>
2019-11-14 20:14:11 -0500

    Next work on Kalman Filter

--- notebook.py
+++ notebook.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+import cv2 as cv
 
 # import sys
 # sys.executable
@@ -52,8 +52,8 @@ T_01 = data.calib.T_01 # Translation to left image
 
 # Undistort images
 
-undistorted_img0 = cv2.undistort(img0_raw, K_00, D_00)
-undistorted_img1 = cv2.undistort(img1_raw, K_01, D_01)
+undistorted_img0 = cv.undistort(img0_raw, K_00, D_00)
+undistorted_img1 = cv.undistort(img1_raw, K_01, D_01)
 
 # "Undistorted Left Image"
 # st.image(undistorted_img0, use_column_width=True)
@@ -69,7 +69,7 @@ Q     = np.zeros((4,4)) # Disparity to depth
 
 # Perform Rectification
 
-cv2.stereoRectify(
+cv.stereoRectify(
     K_00,
     D_00,
     K_01,
@@ -85,8 +85,8 @@ cv2.stereoRectify(
     alpha=0,
 )
 
-mapx_00, mapy_00 = cv2.initUndistortRectifyMap(K_00, D_00, Rr_00, Pr_00, img_size, cv2.CV_32FC1)
-img0_rect = cv2.remap(img0_raw, mapx_00, mapy_00, cv2.INTER_LINEAR)
+mapx_00, mapy_00 = cv.initUndistortRectifyMap(K_00, D_00, Rr_00, Pr_00, img_size, cv.CV_32FC1)
+img0_rect = cv.remap(img0_raw, mapx_00, mapy_00, cv.INTER_LINEAR)
 
 # "Manually Rectfied Left Image"
 # st.image(img0_rect, use_column_width=True)
@@ -94,8 +94,8 @@ img0_rect = cv2.remap(img0_raw, mapx_00, mapy_00, cv2.INTER_LINEAR)
 # "Kitti Rectified Left Image"
 # st.image(img0, use_column_width=True)
 
-mapx_01, mapy_01 = cv2.initUndistortRectifyMap(K_01, D_01, Rr_01, Pr_01, img_size, cv2.CV_32FC1)
-img1_rect = cv2.remap(img1_raw, mapx_01, mapy_01, cv2.INTER_LINEAR)
+mapx_01, mapy_01 = cv.initUndistortRectifyMap(K_01, D_01, Rr_01, Pr_01, img_size, cv.CV_32FC1)
+img1_rect = cv.remap(img1_raw, mapx_01, mapy_01, cv.INTER_LINEAR)
 
 # 'Manually Rectified Right Image'
 # st.image(img1_rect, use_column_width=True)
@@ -104,7 +104,7 @@ img1_rect = cv2.remap(img1_raw, mapx_01, mapy_01, cv2.INTER_LINEAR)
 # st.image(img1, use_column_width=True)
 
 # compute disparities
-stereo = cv2.StereoBM_create()
+stereo = cv.StereoBM_create()
 kitti_disparities = stereo.compute(img0, img1)
 rect_disparities  = stereo.compute(img0_rect, img1_rect)
 
@@ -123,22 +123,45 @@ rect_density  = float((rect_disparities  >= 0).sum()) / float(rect_disparities.s
 # f"Kitti Rectified Pair Disparity Density: {kitti_density:.2%}"
 # f"Manually Rectified Pair Disparity Density: {rect_density:.2%}"
 
+# Start of real algorithm
+
 # Find matching points between left and right images
 
-sift = cv2.xfeatures2d.SIFT_create()
-keypoints = sift.detect((img0_raw, img1_raw), None)
+im0 = undistorted_img0
+im1 = undistorted_img1
 
-img0_points = np.zeros(img0_raw.shape)
-img0_points = cv2.drawKeypoints(img0_raw, keypoints[0], img0_points) 
+sift = cv.xfeatures2d.SIFT_create()
+keypoints = sift.detect((im0, img1_raw), None)
+
+img0_points = np.zeros(im0.shape)
+img0_points = cv.drawKeypoints(im0, keypoints[0], img0_points, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
 
 "Left Image SIFT Matching Points"
 st.image(img0_points, use_column_width=True)
 
-img1_points = np.zeros(img1_raw.shape)
-img1_points = cv2.drawKeypoints(img1_raw, keypoints[1], img1_points)
+img1_points = np.zeros(im1.shape)
+img1_points = cv.drawKeypoints(im1, keypoints[1], img1_points, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 "Right Image SIFT Matching Points"
 st.image(img1_points, use_column_width=True)
 
 f"{len(keypoints[0])} Points Found"
+
+# The re-calibration objective function is the sum of squared epipolar errors
+# See Online Continuous Stereo Extrinsic Parameter Estimation (7), (8)
+
+st.text(K_00)
+st.text(K_01)
+st.text(T_00)
+st.text(T_01)
+
+Kl = K_00
+Kr = K_01
+t  = T_01
+
+def epipolar_error(Rr_t, Rl_t, Kl, Kr, ul, ur, f):
+
+    err = f * Rr_t.T
+    
+
 
